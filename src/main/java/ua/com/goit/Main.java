71dc35baff253652bc1b3bf2147b740fe7578127
Sergii Shynkarenko <atlantiks@outlook.com>
2022-09-06 13:01:24 +0300

    create test table via JDBC

--- src/main/java/ua/com/goit/Main.java
+++ src/main/java/ua/com/goit/Main.java
@@ -2,11 +2,22 @@ package ua.com.goit;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        var x = ConnectionManager.openConnection();
-        System.out.println(x.getTransactionIsolation());
+        String SQL = "CREATE TABLE IF NOT EXISTS developers_test (" +
+                "id SERIAL," +
+                "first_name VARCHAR(255)," +
+                "last_name VARCHAR(255)" +
+                ");";
+        try (Connection connection = ConnectionManager.openConnection();
+             Statement statement = connection.createStatement()) {
+            var x = statement.execute(SQL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
