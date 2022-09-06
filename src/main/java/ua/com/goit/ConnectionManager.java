0d4872b3dce634b594705e9056991d7767f8e76b
Sergii Shynkarenko <atlantiks@outlook.com>
2022-09-06 12:40:05 +0300

    set connection settings with Properties

--- src/main/java/ua/com/goit/ConnectionManager.java
+++ src/main/java/ua/com/goit/ConnectionManager.java
@@ -1,22 +1,37 @@
 package ua.com.goit;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class ConnectionManager {
-    private static final String USERNAME = System.getProperty("user");
-    private static final String PASSWORD = System.getProperty("password");
-    private static final String URL = "jdbc:postgresql://host.docker.internal:49153/go_it";
+    private static final Properties PROPERTIES = new Properties();
 
     private ConnectionManager() {
     }
 
     public static Connection openConnection() {
+        initVariables();
         try {
-            return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            return DriverManager.getConnection(
+                    PROPERTIES.getProperty("db.url"),
+                    PROPERTIES.getProperty("user"),
+                    PROPERTIES.getProperty("password"));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void initVariables() {
+        try (var paramStream = ConnectionManager.class.getClassLoader()
+                             .getResourceAsStream("application.properties")) {
+            PROPERTIES.load(paramStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        PROPERTIES.setProperty("user",System.getProperty("user"));
+        PROPERTIES.setProperty("password",System.getProperty("password"));
     }
 }
