a088c583e626f50253ec0d54833201259a544616
Sergii Shynkarenko <atlantiks@outlook.com>
2022-10-06 15:03:39 +0300

    Javax Servlets swapped to Jakarta

--- src/main/java/ua/com/goit/servlet/FirstServlet.java
+++ src/main/java/ua/com/goit/servlet/FirstServlet.java
@@ -1,21 +1,23 @@
 package ua.com.goit.servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/")
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet("/")
 public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("html/text");
         try (var writer = resp.getWriter()) {
-            writer.write("<H1>Some text...</H1>");
+            writer.write("<h1>Some text here...</h1>");
+            writer.write("<h1>" + LocalDateTime.now() +"</h1>");
         }
-        //req.getRequestDispatcher("/html/first.html").forward(req,resp);
     }
 }
