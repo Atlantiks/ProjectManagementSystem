0d482fb790d5da954e90677937939409ba97900e
Sergii Shynkarenko <atlantiks@outlook.com>
2022-12-01 23:02:26 +0200

    update customer validation

--- src/main/java/ua/com/goit/validation/CreateCustomerValidator.java
+++ src/main/java/ua/com/goit/validation/CreateCustomerValidator.java
@@ -15,8 +15,8 @@ public class CreateCustomerValidator implements Validator<CreateCustomerDto> {
 
     @Override
     public boolean isValid(CreateCustomerDto customerDto) {
-        if (customerDto.getName().isBlank()) return false;
-        if (customerDto.getSurname().isBlank()) return false;
+        if (!customerDto.getName().matches("[A-z\\s]+")) return false;
+        if (!customerDto.getSurname().matches("[A-z]+")) return false;
         if (customerDto.getAddress().isBlank()) return false;
 
         return true;
